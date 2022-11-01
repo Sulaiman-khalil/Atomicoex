3749791083bbfdd95e8d575869e41c0c65869b08
Sulaiman Khalil <sulaiman.khalil@ibm.com>
2022-11-01 10:45:28 +0100

    delete

--- src/icon/iconHome.tsx
+++ src/icon/iconHome.tsx
@@ -1,39 +1,39 @@
-import { Props, c } from "atomico";
+// import { Props, c } from "atomico";
 
-function iconHome({ color, width, height, data }: Props<typeof iconHome>) {
-    console.log(color);
-    return (
-        <host>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={width}
-                height={height}
-                viewBox="0 0 24 24"
-            >
-                <path d={data} fill={color} />
-            </svg>
-        </host>
-    );
-}
+// function iconHome({ color, width, height, data }: Props<typeof iconHome>) {
+//     console.log(color);
+//     return (
+//         <host>
+//             <svg
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 width={width}
+//                 height={height}
+//                 viewBox="0 0 24 24"
+//             >
+//                 <path d={data} fill={color} />
+//             </svg>
+//         </host>
+//     );
+// }
 
-iconHome.props = {
-    color: {
-        type: String,
-        value: "rgb(109, 109, 109)",
-    },
-    width: {
-        type: String,
-        value: "1.5rem",
-    },
-    height: {
-        type: String,
-        value: "1.5rem",
-    },
-    data: {
-        type: String,
-    },
-};
+// iconHome.props = {
+//     color: {
+//         type: String,
+//         // value: "#6d6d6d",
+//     },
+//     width: {
+//         type: String,
+//         value: "1.5rem",
+//     },
+//     height: {
+//         type: String,
+//         value: "1.5rem",
+//     },
+//     data: {
+//         type: String,
+//     },
+// };
 
-export const IconHome = c(iconHome);
+// export const IconHome = c(iconHome);
 
-customElements.define("icon-home", IconHome);
+// customElements.define("icon-home", IconHome);
